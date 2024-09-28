b05612b1b7b46cdb8c5f5b195a9bf86b894ed4dd
Slayer <slayerpro27@gmail.com>
2024-09-28 20:42:14 +0300

    Re-added broken session handling

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -51,6 +51,11 @@ class Tapper:
 
         self._webview_data = None
 
+        if self.proxy:
+            proxy = Proxy.from_str(self.proxy)
+            proxy_dict = proxy_utils.to_telethon_proxy(proxy)
+            self.tg_client.set_proxy(proxy_dict)
+
     def log_message(self, message) -> str:
         return f"<light-yellow>{self.session_name}</light-yellow> | {message}"
 
@@ -63,36 +68,37 @@ class Tapper:
 
         return user_agent
 
+    async def initialize_webview_data(self):
+        if not self._webview_data:
+            while True:
+                try:
+                    peer = await self.tg_client.get_input_entity('b_usersbot')
+                    input_bot_app = InputBotAppShortName(bot_id=peer, short_name="join")
+                    self._webview_data = {'peer': peer, 'app': input_bot_app}
+                    break
+                except FloodWaitError as fl:
+                    fls = fl.seconds
+
+                    logger.warning(self.log_message(f"FloodWait {fl}. Waiting {fls}s"))
+                    await asyncio.sleep(fls + 3)
+
+                except (UnauthorizedError, AuthKeyUnregisteredError):
+                    raise InvalidSession(f"{self.session_name}: User is unauthorized")
+
+                except (UserDeactivatedError, UserDeactivatedBanError, PhoneNumberBannedError):
+                    raise InvalidSession(f"{self.session_name}: User is banned")
+
     async def get_tg_web_data(self) -> str | None:
-
-        if self.proxy:
-            proxy = Proxy.from_str(self.proxy)
-            proxy_dict = proxy_utils.to_telethon_proxy(proxy)
-        else:
-            proxy_dict = None
-
-        self.tg_client.set_proxy(proxy_dict)
-
         init_data = None
         with self.lock:
-            async with self.tg_client as client:
-                if not self._webview_data:
-                    while True:
-                        try:
-                            peer = await client.get_input_entity('b_usersbot')
-                            input_bot_app = InputBotAppShortName(bot_id=peer, short_name="join")
-                            self._webview_data = {'peer': peer, 'app': input_bot_app}
-                            break
-                        except FloodWaitError as fl:
-                            fls = fl.seconds
-
-                            logger.warning(self.log_message(f"FloodWait {fl}"))
-                            logger.info(self.log_message(f"Sleep {fls}s"))
-                            await asyncio.sleep(fls + 3)
+            try:
+                if not self.tg_client.is_connected():
+                    await self.tg_client.connect()
+                await self.initialize_webview_data()
 
                 ref_id = settings.REF_ID if random.randint(0, 100) <= 85 else "ref-4LKnoTn1gnxdSFUDGoyBLr"
 
-                web_view = await client(messages.RequestAppWebViewRequest(
+                web_view = await self.tg_client(messages.RequestAppWebViewRequest(
                     **self._webview_data,
                     platform='android',
                     write_allowed=True,
@@ -115,8 +121,19 @@ class Tapper:
                 init_data = (f"user={user_data_encoded}&chat_instance={chat_instance}&chat_type={chat_type}&"
                              f"start_param={start_param}&auth_date={auth_date}&hash={hash_value}")
 
-                me = await client.get_me()
+                me = await self.tg_client.get_me()
                 self.tg_client_id = me.id
+
+            except InvalidSession:
+                raise
+
+            except Exception as error:
+                log_error(self.log_message(f"Unknown error during Authorization: {error}"))
+                await asyncio.sleep(delay=3)
+
+            finally:
+                if self.tg_client.is_connected():
+                    await self.tg_client.disconnect()
 
         return init_data
 
@@ -291,8 +308,8 @@ class Tapper:
                                     f"Reward: <lc>+{task.get('secondsAmount')}</lc>"))
                         await asyncio.sleep(delay=5)
 
-                except InvalidSession as error:
-                    raise error
+                except InvalidSession:
+                    raise
 
                 except Exception as error:
                     log_error(self.log_message(f"Unknown error: {error}"))
